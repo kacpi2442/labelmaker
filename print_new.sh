f79700b1fdac9efe574aba51d8e72b2e81655c1b
kacpi2442 <git@2442.pl>
2024-04-02 19:41:16 +0200

    Add conditional logic to handle cable input in print_new.sh

--- print_new.sh
+++ print_new.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# if $2 defined
+if [ -z "$2" ]
+then
+    cable=0
+else
+    cable=1
+    text=$2
+fi
+
 for i in $(seq 1 $1)
 do
-    python3 ../inventory-codegen/meow.py && python3 labelmaker.py -i barcode.png -F True -a True -C 98:6E:E8:43:E1:0C
+    python3 ../inventory-codegen/meow.py $cable "$text" && python3 labelmaker.py -i barcode.png -F True -a True -C 98:6E:E8:43:E1:0C
     sleep 1
 done
